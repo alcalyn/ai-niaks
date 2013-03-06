bd820e20f55e78c145b210c0a0c4cf15f8e66380
maxime.julien.l3@gmail.com <maxime.julien.l3@gmail.com@f7a4a791-6b07-43bc-1b1a-5b9cafaf62df>
2013-03-06 16:26:31 +0000

    hello world

--- trunk/src/appli/Appli.java
+++ trunk/src/appli/Appli.java
@@ -5,15 +5,15 @@ import model.Partie;
 
 public class Appli {
 
-	
-	
+	//hello world
+
 	public static void main(String[] args) {
-		
+
 		int taille_plateau = 4;
 		int nb_joueur = 2;
-		
+
 		Partie partie = new Partie(nb_joueur, taille_plateau);
-		
+
 		NiaksFrame frame = new NiaksFrame(partie);
 	}
 
