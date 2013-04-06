f2e1837844821b762fe82a5c35b00ea3e1a03bd5
julien.maulny@gmail.com <julien.maulny@gmail.com@f7a4a791-6b07-43bc-1b1a-5b9cafaf62df>
2013-04-06 10:46:35 +0000

    afficher les coups sur la minimaxView

--- src/views/Chemin.java
+++ src/views/Chemin.java
@@ -8,23 +8,28 @@ import model.OriginRotation;
 
 public class Chemin {
 	
-	private static final int passage_size = 12;
+	private int passage_size = 12;
+	private int cell_spacing = PlateauPanel.cell_spacing;
 	private static final Color passage_color = Color.RED;
 	private static final Color line_color = Color.RED;
 	
 	
 	
-	private OriginRotation plateau_panel;
+	private OriginRotation abstractPlateau;
 	private Coords [] chemin;
 	private int taille;
 	
 	
-	public Chemin(OriginRotation plateau_panel, Coords[] chemin) {
-		this.plateau_panel = plateau_panel;
+	public Chemin(OriginRotation abstractPlateau, Coords[] chemin) {
+		this.abstractPlateau = abstractPlateau;
 		this.chemin = chemin;
 		this.taille = chemin.length;
 	}
 	
+	public void setGraphicsDimension(int passage_size, int cell_spacing) {
+		this.passage_size = passage_size;
+		this.cell_spacing = cell_spacing;
+	}
 	
 	public void drawChemin(Graphics g) {
 		for(int i=1;i<taille;i++) {
@@ -38,27 +43,27 @@ public class Chemin {
 	
 	
 	private void drawPassage(Graphics g, Coords p) {
-		Coords pw = p.mul(PlateauPanel.cell_spacing).toWindow(plateau_panel.getRotation());
+		Coords pw = p.mul(cell_spacing).toWindow(abstractPlateau.getRotation());
 		
 		g.setColor(passage_color);
 		g.fillOval(
-				pw.x - passage_size / 2 + plateau_panel.getOrigin().x,
-				pw.y - passage_size / 2 + plateau_panel.getOrigin().y,
+				pw.x - passage_size / 2 + abstractPlateau.getOrigin().x,
+				pw.y - passage_size / 2 + abstractPlateau.getOrigin().y,
 				passage_size,
 				passage_size
 		);
 	}
 	
 	private void drawLine(Graphics g, Coords p0, Coords p1) {
-		Coords w0 = p0.mul(PlateauPanel.cell_spacing).toWindow(plateau_panel.getRotation());
-		Coords w1 = p1.mul(PlateauPanel.cell_spacing).toWindow(plateau_panel.getRotation());
+		Coords w0 = p0.mul(cell_spacing).toWindow(abstractPlateau.getRotation());
+		Coords w1 = p1.mul(cell_spacing).toWindow(abstractPlateau.getRotation());
 		
 		g.setColor(line_color);
 		g.drawLine(
-				w0.x + plateau_panel.getOrigin().x,
-				w0.y + plateau_panel.getOrigin().y,
-				w1.x + plateau_panel.getOrigin().x,
-				w1.y + plateau_panel.getOrigin().y
+				w0.x + abstractPlateau.getOrigin().x,
+				w0.y + abstractPlateau.getOrigin().y,
+				w1.x + abstractPlateau.getOrigin().x,
+				w1.y + abstractPlateau.getOrigin().y
 		);
 				
 	}
