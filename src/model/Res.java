92033a8fe3453e0f119b370b971d836ddaca2f6b
julien.maulny@gmail.com <julien.maulny@gmail.com@f7a4a791-6b07-43bc-1b1a-5b9cafaf62df>
2013-04-26 09:34:58 +0000

    Ressources manager

---
+++ src/model/Res.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class Res {
+	
+	
+	
+	private static URL getRes(String res) {
+		return new Res().getClass().getResource("/"+res);
+	}
+	
+	private static InputStream getResStream(String res) {
+		return new Res().getClass().getResourceAsStream("/"+res);
+	}
+	
+	
+	
+	public static String getFileContent(String r) {
+		String content = null;
+		
+		try {
+			BufferedInputStream bis = new BufferedInputStream(getResStream(r));
+			byte[] chars = new byte[(int) bis.available()];
+			bis.read(chars);
+			content = new String(chars);
+			bis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return content;
+	}
+	
+	public static Image getImage(String res) {
+		ImageIcon img = new ImageIcon(getRes(res));
+		return img.getImage();
+	}
+	
+	public static Icon getIcon(String res) {
+		return new ImageIcon(getRes(res));
+	}
+	
+	public static Icon getIcon(String res, Dimension dimension) {
+		return new ImageIcon(getImage(res).getScaledInstance(dimension.width, dimension.height, Image.SCALE_SMOOTH));
+	}
+	
+	
+}
